b4964fb8393142bba03b583c6a4d89b204286852
manh-trinhquoc <34735335+manh-trinhquoc@users.noreply.github.com>
2019-07-24 15:10:13 +0700

    release v1.0 hot fix respnsive problem

--- bat/git.bat
+++ bat/git.bat
@@ -1,8 +1,8 @@
 cd F:\thang-long-tour
 git add .
-git commit -m "release v1.0 test git"
+git commit -m "release v1.0 hot fix respnsive problem"
 git push
 cd F:\manh-trinhquoc.github.io
 git add .
-git commit -m "thang-long-tour: release v1.0: test git"
+git commit -m "thang-long-tour: release v1.0: hot fix respnsive problem"
 git push
