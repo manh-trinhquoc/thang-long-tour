44e2b182587ecb64d3e82a5c1474b1879a8ef719
manh-trinhquoc <34735335+manh-trinhquoc@users.noreply.github.com>
2019-07-25 12:19:42 +0700

    Update 2019-07-25T05:19:09.223Z

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var browserSync = require('browser-sync');
 
 
 function sass() {
+    // compile sass file to css. include map file
     return src('app/scss/**')
         .pipe(sourcemaps.init())
         .pipe(SASS().on('error', SASS.logError)) // Using gulp-sass
@@ -15,11 +16,13 @@ function sass() {
 }
 
 function watchSaas(cb) {
+    // monitor scss file
     watch(['app/**', '!app/css/**'], { ignoreInitial: false }, sass);
     cb();
 }
 
 exports.default = function(cb) {
+    // compile sass and reload browser if anything change
     browserSync.init({
         server: {
             baseDir: 'app'
@@ -35,14 +38,44 @@ exports.default = function(cb) {
 
 
 exports.browserSync = function() {
+    // reload browser if anything change. exclude scss file
     browserSync.init({
         server: {
             baseDir: 'app'
         },
-    })
+    });
 
     watch(["app/**", '!app/css/**', '!app/scss/**'], function(cb) {
         browserSync.reload();
         cb();
     });
+};
+
+function copy() {
+    return src(['app/**/*', '!app/scss/**'], {
+        base: 'app'
+    }).pipe(dest('F://gulp-build/'));
 }
+
+exports.build = copy;
+
+/**
+ * Push to git hub width ghPages
+ */
+let ghPages = require('gulp-gh-pages');
+
+exports.github = function() {
+    return src(['**/*', '.gitignore', '!node_modules/', '!.git', '!package-lock.json'])
+        .pipe(ghPages({ branch: 'master' }))
+};
+
+// git with gulp-git
+// let git = require('gulp-git');
+// Run git add
+// src is the file(s) to add (or ./*)
+// function add() {
+//     return src('./*')
+//         .pipe(git.add());
+// }
+
+// exports.gitadd = add;
