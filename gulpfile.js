c89f9acc7280e815984a365ad61eadc318eac738
manh-trinhquoc <34735335+manh-trinhquoc@users.noreply.github.com>
2019-08-02 08:51:48 +0700

    auto beautiful

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,63 @@
 // variable change depend on project
-let remotePath = 'F://server/gulp-build/';
-let commitMessage = 'gulp build release. have work flow with auto clean, copy and push github';
+let remotePath = "F://server/gulp-build/";
+let commitMessage = "auto beautiful";
 
+//
+const { src, dest, watch, series, parallel } = require("gulp");
 
-// 
-const { src, dest, watch, series, parallel } = require('gulp');
+let SASS = require("gulp-sass");
+let sourcemaps = require("gulp-sourcemaps");
 
-let SASS = require('gulp-sass');
-let sourcemaps = require('gulp-sourcemaps');
-
-let browserSync = require('browser-sync');
-
+let browserSync = require("browser-sync");
 
 function sass() {
-    // compile sass file to css. include map file
-    return src('app/scss/**')
-        .pipe(sourcemaps.init())
-        .pipe(SASS().on('error', SASS.logError)) // Using gulp-sass
-        .pipe(sourcemaps.write(''))
-        .pipe(dest('app/css/'))
+  // compile sass file to css. include map file
+  return src("app/scss/**")
+    .pipe(sourcemaps.init())
+    .pipe(SASS().on("error", SASS.logError)) // Using gulp-sass
+    .pipe(sourcemaps.write(""))
+    .pipe(dest("app/css/"));
 }
 
 function watchSaas(cb) {
-    // monitor scss file
-    watch(['app/**', '!app/css/**'], { ignoreInitial: false }, sass);
-    cb();
+  // monitor scss file
+  watch(["app/**", "!app/css/**"], { ignoreInitial: false }, sass);
+  cb();
 }
 
 exports.default = function(cb) {
-    // compile sass and reload browser if anything change
-    browserSync.init({
-        server: {
-            baseDir: 'app'
-        },
-    })
-    watch(['app/**', '!app/css/**'], { ignoreInitial: false }, sass);
-    watch(["app/**", '!app/scss/**'], function(cb) {
-        browserSync.reload();
-        cb();
-    });
+  // compile sass and reload browser if anything change
+  browserSync.init({
+    server: {
+      baseDir: "app"
+    }
+  });
+  watch(["app/**", "!app/css/**"], { ignoreInitial: false }, sass);
+  watch(["app/**", "!app/scss/**"], function(cb) {
+    browserSync.reload();
     cb();
-}
-
+  });
+  cb();
+};
 
 exports.browserSync = function() {
-    // reload browser if anything change. exclude scss file
-    browserSync.init({
-        server: {
-            baseDir: 'app'
-        },
-    });
+  // reload browser if anything change. exclude scss file
+  browserSync.init({
+    server: {
+      baseDir: "app"
+    }
+  });
 
-    watch(["app/**", '!app/css/**', '!app/scss/**'], function(cb) {
-        browserSync.reload();
-        cb();
-    });
+  watch(["app/**", "!app/css/**", "!app/scss/**"], function(cb) {
+    browserSync.reload();
+    cb();
+  });
 };
 
 function copy() {
-    return src(['app/**/*', '!app/scss/**'], {
-        base: 'app'
-    }).pipe(dest(remotePath));
+  return src(["app/**/*", "!app/scss/**"], {
+    base: "app"
+  }).pipe(dest(remotePath));
 }
 
 exports.copy = copy;
@@ -76,30 +73,31 @@ exports.copy = copy;
 // };
 
 // git with gulp-git
-let git = require('gulp-git');
+let git = require("gulp-git");
 // Run git add
 // src is the file(s) to add (or ./*)
 function add() {
-    return src('.')
-        .pipe(git.add());
+  return src(".").pipe(git.add());
 }
 
 exports.gitadd = add;
 
 function commit() {
-    return src('./')
-        // ********************************************************************
-        // commit develope folder
-        .pipe(git.commit(commitMessage));
+  return (
+    src("./")
+      // ********************************************************************
+      // commit develope folder
+      .pipe(git.commit(commitMessage))
+  );
 }
 
 exports.gitcommit = commit;
 
 function push(cb) {
-    git.push('origin', 'master', function(err) {
-        if (err) throw err;
-    });
-    cb();
+  git.push("origin", "master", function(err) {
+    if (err) throw err;
+  });
+  cb();
 }
 
 exports.gitpush = push;
@@ -107,32 +105,33 @@ exports.gitpush = push;
 //do 3 action in series
 exports.github = series(add, commit, push);
 
-// git in a different folder 
+// git in a different folder
 
 function addremote() {
-    process.chdir(remotePath);
-    return src('.')
-        .pipe(git.add());
+  process.chdir(remotePath);
+  return src(".").pipe(git.add());
 }
 
 exports.gitaddremote = addremote;
 
 function commitremote() {
-    process.chdir(remotePath);
-    return src('./')
-        // ********************************************************************
-        // commit distribution folder
-        .pipe(git.commit(commitMessage));
+  process.chdir(remotePath);
+  return (
+    src("./")
+      // ********************************************************************
+      // commit distribution folder
+      .pipe(git.commit(commitMessage))
+  );
 }
 
 exports.gitcommitremote = commitremote;
 
 function pushremote(cb) {
-    process.chdir(remotePath);
-    git.push('origin', 'master', function(err) {
-        if (err) throw err;
-    });
-    cb();
+  process.chdir(remotePath);
+  git.push("origin", "master", function(err) {
+    if (err) throw err;
+  });
+  cb();
 }
 
 exports.gitpushremote = pushremote;
@@ -141,17 +140,19 @@ exports.gitpushremote = pushremote;
 exports.githubremote = series(addremote, commitremote, pushremote);
 
 // delete files programmatically
-let del = require('del');
+let del = require("del");
 
 function clean() {
-    return del([remotePath + '**/*'], {
-        force: true
-    });
+  return del([remotePath + "**/*"], {
+    force: true
+  });
 }
 
 exports.clean = clean;
 
 //build task include everything
 
-exports.build = series(series(add, commit, push), series(clean, copy,
-    addremote, commitremote, pushremote));
+exports.build = series(
+  series(add, commit, push),
+  series(clean, copy, addremote, commitremote, pushremote)
+);
